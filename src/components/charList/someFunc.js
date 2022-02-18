6ca0c5b8c40e50fecacf2348af136e3e8d78b339
Tokmianin Alex <tokafrag@ya.ru>
2022-02-18 21:33:41 +0500

    React приложение на Marvel API

---
+++ src/components/charList/someFunc.js
@@ -0,0 +1,7 @@
+export function logger() {
+	console.log('хуйло');
+}
+
+export function secondLog() {
+	console.log('Хуйло два раза');
+}
---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,13 @@
+import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
+
+const ComicsPage = () => {
+	return (
+		<>
+			<ComicsList/>
+			<AppBanner/>
+		</>
+	)
+}
+
+export default ComicsPage;
